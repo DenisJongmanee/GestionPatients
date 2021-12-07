44b3c15550a3b64b0b739393e58d99250ff76503
PAUL BAMMEZ <bammezpaul@gmail.com>
2021-12-07 18:15:02 +0100

    forulaires modification

--- src/Controller/ModifierPatientController.php
+++ src/Controller/ModifierPatientController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\PatientType;
 
 class ModifierPatientController extends AbstractController
 {
     #[Route('/modifier_patient', name: 'modifier_patient')]
     public function index(): Response
     {
+        $form = $this->createForm(PatientType::class);
+
         return $this->render('modifier_patient/index.html.twig', [
-            'controller_name' => 'ModifierPatientController',
+            'CreationPatientController' => $form->createView(),
         ]);
     }
 }
