d09a686cc9f7f85fa5de4227e34d19d9f9a8a287
DenisJongmanee <jongmanee.denis@gmail.com>
2021-12-07 21:26:24 +0100

    add list sejour

--- src/Controller/SejourController.php
+++ src/Controller/SejourController.php
@@ -2,22 +2,28 @@
 
 namespace App\Controller;
 
-use App\Form\SejourType;
+use App\Entity\Service;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Service\SejourService;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class SejourController extends AbstractController
 {
+    private $client;
+    public function __construct(HttpClientInterface $client)
+    {
+        $this -> client = $client;
+    }
     #[Route('/sejour', name: 'sejour')]
-    public function index(): Response
+    public function index(SejourService $api): Response
     {
 
-        $form = $this->createForm(SejourType::class);
-
+        $sejours = $api->getSejours();
+        
         return $this->render('sejour/index.html.twig', [
-            'SejourController' => $form->createView(),
+            "sejours" => $sejours
         ]);
     }
 }
---
+++ src/Service/SejourService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpClient\HttpClient;
+
+
+class SejourService
+{
+    private $httpclient;
+
+    public function __construct(HttpClientInterface $httpclient)
+    {
+        $this->httpclient = $httpclient;
+        $this->url = 'http://127.0.0.1:5000/api/sejour';
+    }
+
+    public function getApi(string $param = "")
+    {
+        if ($param != "") $param = "/" . $param; 
+        
+    
+        return $this->httpclient->request(
+            'GET',
+            $this->url . $param
+        );
+    }
+    
+    public function getSejours() {
+        $response = $this->getApi();
+        if ($response->getStatusCode() == 200) {
+            return $response->toArray();
+        }
+    }
+
+    public function getSejour(string $id) {
+        $response = $this->getApi($id);
+
+        if ($response->getStatusCode() == 200) {
+            return $response->toArray();
+        }
+    }
+
+
+}
